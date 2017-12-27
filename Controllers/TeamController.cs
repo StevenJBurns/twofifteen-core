db897ac0a08adc474b150d64eb570697ea759b8a
stevenjburns@msn.com <stevenjburns@msn.com>
2017-12-27 16:19:12 -0700

    Added controllers for most classes

---
+++ Controllers/TeamController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SJB.TwoFifteen.Controllers
+{
+  public class TeamController: Controller
+  {
+
+
+  }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
