b6108ba8ad6812d677d7b4f49b84f095e2cf9df8
stevenjburns@msn.com <stevenjburns@msn.com>
2017-12-27 12:41:44 -0700

    Repo cloned to Dell desktop successfully; Created Models for Franchise, Team and Venue

---
+++ Models/Venue.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SJB.TwoFifteen.Models
+{
+  public class Venue
+  {
+    public int ID { get; set; }
+    public int Location { get; set; }
+    public String Name { get; set; }
+  }
+}
