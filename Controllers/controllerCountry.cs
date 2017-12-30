2f2546a3a2a44fdae3cdd7b1c840d1ff5dae557b
stevenjburns@msn.com <stevenjburns@msn.com>
2017-12-30 12:38:41 -0700

    HTTP GET works for country and country-id; Trimmed countries long_name field of trailing spaces; very big step forward

--- Controllers/controllerCountry.cs
+++ Controllers/controllerCountry.cs
@@ -3,12 +3,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using SJB.TwoFifteen.DataAccessLayer;
+using SJB.TwoFifteen.Models;
 
 namespace SJB.TwoFifteen.Controllers
-{
-  public class CountryController : Controller
   {
+  [Route("/[controller]")]
+  public class CountryController : Controller
+    {
+    private readonly TwoFifteenContext _context;
 
+    public CountryController(TwoFifteenContext context)
+      {
+      _context = context;
+      
+      if (_context.Countries.Count() == 0)
+        {
+        // _context.Countries.Add(new TodoItem { Name = "Item1" });
+        // _context.SaveChanges();
+        }
+      }
 
+    [HttpGet]
+    public IEnumerable<Country> GetAll()
+      { return _context.Countries.ToList(); }
+
+    [HttpGet("{id}", Name = "GetCountry")]
+    public IActionResult GetById(Int16 id)
+      {
+      var country = _context.Countries.FirstOrDefault(c => c.ID == id);
+
+      if (country == null) return NotFound();
+    
+      return new ObjectResult(country);
+      }
+    }
   }
-}
--- DataAccessLayer/contextTwoFifteenDB.cs
+++ DataAccessLayer/contextTwoFifteenDB.cs
@@ -12,5 +12,6 @@ namespace SJB.TwoFifteen.DataAccessLayer
     public DbSet<Franchise> Franchises { get; set; }
     public DbSet<Team> Teams { get; set; }
     public DbSet<Venue> Venues { get; set; }
+    public DbSet<Country> Countries { get; set; }
     }
 }
