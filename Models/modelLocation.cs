9afa498e2a2732efb445eb7f29db293e0d697d6b
stevenjburns@msn.com <stevenjburns@msn.com>
2018-01-03 15:01:37 -0700

    Added model for Location to match database

---
+++ Models/modelLocation.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SJB.TwoFifteen.Models
+  {
+  public class Location
+    {
+    Int16 ID { get; set };
+    String City { get; set; };
+    String State { get; set; };
+    String StateLong { get; set; };
+    String Country { get; set; };
+
+
+
+    }
+  }
