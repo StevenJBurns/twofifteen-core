331e8fd9e8fbf453dddfa17dfe68bebf4d9031e3
stevenjburns@msn.com <stevenjburns@msn.com>
2018-01-10 16:24:30 -0700

    IIS working again locally; locations API works

--- Models/modelCountry.cs
+++ Models/modelCountry.cs
@@ -6,7 +6,7 @@ namespace SJB.TwoFifteen.Models
   public class Country
   {
     public Int16 ID { get; set; }
-    [Column("name_long", TypeName="nvarchar")]
+    [Column("name_long", TypeName="nvarchar()")]
     public String Name { get; set; }
   }
 }
