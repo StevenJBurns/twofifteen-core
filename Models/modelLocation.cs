cfe87e2d7271f9c2070fef77173252c0eb8e75c2
stevenjburns@msn.com <stevenjburns@msn.com>
2018-01-10 15:54:32 -0700

    Trying to get IIS to run... again

--- Models/modelLocation.cs
+++ Models/modelLocation.cs
@@ -1,14 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SJB.TwoFifteen.Models
   {
   public class Location
     {
     public Int16 ID { get; set; }
-    public String City { get; set; }
-    public String StateLong { get; set; }
-    public String StateShort { get; set; }
-    public String CountryLong { get; set; }
+    [Column("state_long", TypeName="nvarchar")]
+    public string City { get; set; }
+    [Column("state_long", TypeName="nvarchar")]
+    public string StateLong { get; set; }
+    [Column("state_short", TypeName="nvarchar")]
+    public string StateShort { get; set; }
+    [Column("country_long", TypeName="nvarchar")]
+    public string CountryLong { get; set; }
+    [Column("state_short", TypeName="nvarchar")]
     public String CountryShort { get; set; }
     }
   }
