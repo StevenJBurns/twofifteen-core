d3ffb986e678af6cdd7c14de1af258cb6f6b977a
stevenjburns@msn.com <stevenjburns@msn.com>
2018-01-12 15:23:47 -0700

    Fixed relational data queries

--- Controllers/controllerVenue.cs
+++ Controllers/controllerVenue.cs
@@ -3,12 +3,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SJB.TwoFifteen.DataAccessLayer;
+using SJB.TwoFifteen.Models;
 
 namespace SJB.TwoFifteen.Controllers
-{
-  public class VenueController : Controller
   {
+  [Route("/[controller]")]
+  public class VenueController : Controller
+    {
+    private readonly TwoFifteenContext _context;
 
+    public VenueController(TwoFifteenContext context)
+      {
+      _context = context;
+      
+      if (_context.Locations.Count() == 0)
+        {
+        // _context.Countries.Add(new TodoItem { Name = "Item1" });
+        // _context.SaveChanges();
+        }
+      }
 
+    [HttpGet]
+    public IEnumerable<Venue> GetAll()
+      {
+      var venues = _context.Venues.Include(v => v.Location).ToList();
+
+      return venues;
+      }
+
+    [HttpGet("{id}", Name = "GetVenue")]
+    public IActionResult GetById(Int32 id)
+      {
+      var venue = _context.Venues.FirstOrDefault(v => v.ID == id);
+
+      if (venue == null) return NotFound();
+    
+      return new ObjectResult(venue);
+      }
+    }
   }
-}
--- DataAccessLayer/contextTwoFifteenDB.cs
+++ DataAccessLayer/contextTwoFifteenDB.cs
@@ -2,18 +2,25 @@ using Microsoft.EntityFrameworkCore;
 using SJB.TwoFifteen.Models;
 
 namespace SJB.TwoFifteen.DataAccessLayer
-{
+  {
   public class TwoFifteenContext : DbContext
     {
-    public TwoFifteenContext(DbContextOptions<TwoFifteenContext> options) : base(options)
-      {
-      }
-
     public DbSet<Location> Locations { get; set; }
     public DbSet<Venue> Venues { get; set; }
     public DbSet<Franchise> Franchises { get; set; }
     public DbSet<Team> Teams { get; set; }
     //public DbSet<Player> Players { get; set; }
+    //public DbSet<Achievement> Achievements { get; set; }
     public DbSet<Country> Countries { get; set; }
+
+    public TwoFifteenContext(DbContextOptions<TwoFifteenContext> options) : base(options)
+      {
+      }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+      {
+      modelBuilder.Entity<Venue>()
+                  .HasOne(v => v.Location);
+      }
     }
-}
+  }
