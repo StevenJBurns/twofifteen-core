a9d3e01a3bfbd03126d7b4de2a9b10c481c8b536
stevenjburns@msn.com <stevenjburns@msn.com>
2018-01-12 15:23:47 -0700

    Fixed relational data queries

--- Models/modelLocation.cs
+++ Models/modelLocation.cs
@@ -1,21 +1,31 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SJB.TwoFifteen.Models
   {
+  [Table("locations")]
   public class Location
     {
     [Column("id", TypeName="int")]
     public Int32 ID { get; set; }
+
     [Column("city", TypeName="nvarchar()")]
     public string City { get; set; }
+
     [Column("state_long", TypeName="nvarchar()")]
     public string StateLong { get; set; }
+
     [Column("state_short", TypeName="nvarchar()")]
     public string StateShort { get; set; }
+
     [Column("country_long", TypeName="nvarchar()")]
     public string CountryLong { get; set; }
+
     [Column("country_short", TypeName="nvarchar()")]
     public String CountryShort { get; set; }
+
+    //public virtual List<Venue> Venues { get; set; }
     }
   }
