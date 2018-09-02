068210a673e4fca8691a74da72babfc94d33fce3
Steven J Burns <stevenjburns@msn.com>
2018-09-01 20:47:47 -0600

    Re-ordered Usings in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using SJB.TwoFifteen.API.DataAccessLayer;
 
 namespace SJB.TwoFifteen.API
@@ -19,7 +19,7 @@ namespace SJB.TwoFifteen.API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
       {
-      services.AddMvc();
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
       services.AddDbContext<TwoFifteenContext> (options => options.UseSqlServer(Configuration.GetConnectionString("TwoFifteenConnection")));
       }
 
@@ -37,6 +37,7 @@ namespace SJB.TwoFifteen.API
         }
       
       app.UseMvc();
+      app.UseHttpsRedirection();
       app.UseFileServer(enableDirectoryBrowsing: false);
       }
     }
