8248db237402fcb0491fa956edd9a24f426b3498
StevenJBurns <stevenjburns@msn.com>
2020-05-15 00:08:11 -0600

    feat: rewrite good functional controllers for Locations and Venues

---
+++ Controllers/VenuesController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SJB.TwoFifteen.Repositories;
+using SJB.TwoFifteen.Models;
+
+namespace SJB.TwoFifteen.Controllers
+  {
+  [ApiController]
+  [Route("/api/venues")]
+  public class VenuesController : ControllerBase
+    {
+    private readonly VenuesMockData _venues = new VenuesMockData();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Venue>> GetAllVenues()
+      {
+      return Ok(_venues.GetAllVenues());
+      }
+
+    [HttpGet("{id}")]
+    public ActionResult<Venue> GetVenueById(Int32 id)
+      {
+      var venue = _venues.GetVenueById(0);
+
+      if (venue == null) return NotFound();
+    
+      return venue;
+      }
+    }
+  }
