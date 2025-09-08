9eb3f07a37dc4c1bc4a30c6c21fd770f64442d72
SJB <stevenjburns@msn.com>
2025-09-07 21:34:07 -0500

    chore: construct .sln and .csproj files to get TestConsole off the ground for VS 2022

---
+++ src/Presentation.TestConsole/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TwoFifteen.Presentation.TestConsole;
+
+internal static class Program
+{
+  private static void Main()
+  {
+    Console.Clear();
+    Console.WriteLine("TestConsole is alive and ready to WriteLine() !!!");
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+    Console.Clear();
+  }
+}
